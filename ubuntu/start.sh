188eb3d4e0b3c09c4530e8b8fcc26e050dce4423
Andrew Smith <andrew.smith@miracl.com>
2018-02-27 19:08:09 +0000

    tidy up start args and remove /var/lock workaround

--- ubuntu/start.sh
+++ ubuntu/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 . ./env.sh
-docker run $@ --detach --rm --name $MIRACL_CONTAINER -ti --privileged -v /tmp/$(mktemp -d):/run -v /sys/fs/cgroup:/sys/fs/cgroup:ro $MIRACL_RUN_OPTS $MIRACL_CONTAINER
+docker run --detach --rm --name $MIRACL_CONTAINER -ti $MIRACL_RUN_OPTS $MIRACL_CONTAINER
