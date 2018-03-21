48b51e4acc8929d90e5b1d5ffcb4bf3a6e2b1372
Andrew Smith <andrew.smith@miracl.com>
2018-03-21 14:57:17 +0000

    auto detect a tty

--- ubuntu/exec.sh
+++ ubuntu/exec.sh
@@ -2,5 +2,7 @@
 set -e
 . ./env.sh
 
+[ -t 1 ] && opts='-ti' || opts='-i'
+
 cmd="$@"
-docker exec -e "LINES=$(tput lines)" -e "COLUMNS=$(tput cols)" -ti $SSO_DOCKER_CONTAINER sh -c "$cmd"
+docker exec -e "LINES=$(tput lines)" -e "COLUMNS=$(tput cols)" $opts $SSO_DOCKER_CONTAINER sh -c "$cmd"
