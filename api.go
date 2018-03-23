9d4d485352b5b2aeb58cd710650adb77063ed31d
Andrew Smith <andrew.smith@miracl.com>
2018-03-23 10:34:47 +0000

    api development

--- api.go
+++ api.go
@@ -2,6 +2,8 @@ package ssodocker
 
 import (
 	"fmt"
+	"io"
+	"io/ioutil"
 	"os"
 	"os/exec"
 	"path"
@@ -16,7 +18,7 @@ const (
 	Centos       = "centos"
 )
 
-type Service string
+type Host string
 
 const (
 	SSO    = "sso"
@@ -31,18 +33,25 @@ const (
 type Repo string
 
 const (
-	Prod = "prd"
-	Dev  = "dev"
+	Prd = "prd"
+	Dev = "dev"
 )
 
 type Context struct {
 	OpSys  OpSys
 	Env    []string
-	Stdout *os.File
-	Stderr *os.File
+	Stdlog io.Writer
+	Stdout io.Writer
+	Stderr io.Writer
 }
 
-func (ctx Context) Command(cmd string, opts ...string) error {
+func New(opSys OpSys) *Context {
+	return &Context{
+		OpSys: opSys,
+	}
+}
+
+func (ctx *Context) Command(cmd string, opts ...string) error {
 	ctx.log(string(ctx.OpSys))
 	ctx.log("Running Command : `%v %v` in %v", cmd, strings.Join(opts, " "), ctx.path())
 	defer func() { ctx.log("Finished Command") }()
@@ -50,26 +59,85 @@ func (ctx Context) Command(cmd string, opts ...string) error {
 	c := exec.Command(cmd, opts...)
 	c.Dir = ctx.path()
 	c.Env = ctx.env()
-	out, err := c.CombinedOutput()
-	ctx.log(string(out))
-	ctx.logErr(err)
-	return err
+	c.Stdout = ctx.stdout()
+	c.Stderr = ctx.stderr()
+
+	return c.Run()
 }
 
-func (ctx Context) Up(services ...Service) error {
-	return ctx.Command("./compose.sh", prepend(strServices(services), "up", "-d")...)
+func (ctx *Context) Up(services ...Host) error {
+	return ctx.Command("./compose.sh", prepend(strHosts(services), "up", "-d")...)
 }
 
-func (ctx Context) Down(services ...Service) error {
-	return ctx.Command("./compose.sh", prepend(strServices(services), "down")...)
+func (ctx *Context) Down(services ...Host) error {
+	return ctx.Command("./compose.sh", prepend(strHosts(services), "down")...)
 }
 
-func (ctx Context) Repo(repo Repo) error {
+func (ctx *Context) Repo(repo Repo) error {
 	return ctx.Command("./repo.sh", string(repo))
 }
 
-func (ctx Context) Install(pkg string, version string) error {
+func (ctx *Context) Install(pkg string, version string) error {
 	return ctx.Command("./install.sh", pkg, version)
+}
+
+func (ctx *Context) Generate(tmplFile string, out io.Writer) error {
+	revert := ctx.SetStdout(out)
+	defer revert()
+	return ctx.Command("../config/generate.sh", tmplFile)
+}
+
+func (ctx *Context) Copy(dest string, src ...string) error {
+	return ctx.Command("./cp.sh", append(src, dest)...)
+}
+
+func (ctx *Context) Service(name string, action string) error {
+	return ctx.Command("./service.sh", name, action)
+}
+
+func (ctx *Context) Redis(action string) error {
+	return ctx.Command("./redis.sh", action)
+}
+
+func (ctx *Context) path() string {
+	return AbsPath(string(ctx.OpSys))
+}
+
+func (ctx *Context) env() []string {
+	return append(append([]string{}, os.Environ()...), ctx.Env...)
+}
+
+func (ctx *Context) logErr(err error) {
+	if err == nil {
+		return
+	}
+	ctx.log(err.Error())
+}
+
+func (ctx *Context) log(msg string, args ...interface{}) {
+	s := fmt.Sprintf(msg+"\n", args...)
+	ctx.stdlog().Write([]byte(s))
+}
+
+func (ctx *Context) stdlog() io.Writer {
+	if ctx.Stdlog != nil {
+		return ctx.Stdlog
+	}
+	return ioutil.Discard
+}
+
+func (ctx *Context) stdout() io.Writer {
+	if ctx.Stdout != nil {
+		return ctx.Stdout
+	}
+	return ioutil.Discard
+}
+
+func (ctx *Context) stderr() io.Writer {
+	if ctx.Stdout != nil {
+		return ctx.Stderr
+	}
+	return ioutil.Discard
 }
 
 func thisDir() string {
@@ -77,7 +145,7 @@ func thisDir() string {
 	return path.Dir(filename)
 }
 
-func absPath(p string) string {
+func AbsPath(p string) string {
 	return path.Join(thisDir(), p)
 }
 
@@ -85,44 +153,21 @@ func prepend(after []string, before ...string) []string {
 	return append(before, after...)
 }
 
-func (ctx Context) logErr(err error) {
-	if err == nil {
-		return
-	}
-	ctx.log(err.Error())
-}
-
-func (ctx Context) path() string {
-	return absPath(string(ctx.OpSys))
-}
-
-func (ctx Context) env() []string {
-	return append(append([]string{}, os.Environ()...), ctx.Env...)
-}
-
-func (ctx Context) log(msg string, args ...interface{}) {
-	s := fmt.Sprintf(msg+"\n", args...)
-	ctx.stdout().WriteString(s)
-}
-
-func (ctx Context) stdout() *os.File {
-	if ctx.Stdout != nil {
-		return ctx.Stdout
-	}
-	return os.Stdout
-}
-
-func (ctx Context) stderr() *os.File {
-	if ctx.Stdout != nil {
-		return ctx.Stderr
-	}
-	return os.Stderr
-}
-
-func strServices(svs []Service) []string {
+func strHosts(svs []Host) []string {
 	s := []string{}
 	for _, sv := range svs {
 		s = append(s, string(sv))
 	}
 	return s
+}
+
+func (ctx *Context) SetStdout(newOut io.Writer) func() {
+	if newOut == nil {
+		return func() {}
+	}
+	oldOut := ctx.Stdout
+	ctx.Stdout = newOut
+	return func() {
+		ctx.Stdout = oldOut
+	}
 }
