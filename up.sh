6d937fa87b23f9258ecf7cab21e88f0eb0355ec1
Andrew Smith <andrew.smith@miracl.com>
2018-03-27 15:59:36 +0100

    cant use volume mappings with balabit/syslog-ng from python docker-compose for some reason

--- up.sh
+++ up.sh
@@ -40,10 +40,10 @@ VERSION=$(./version.sh srv-idp)
 echo $VERSION
 if [[ $VERSION == 1.* ]]; then
   REMOTECONFIG=srv-idp.1.x.remote.json
-  LOCALCONFIG=srv-idp.1.x.json
+  LOCALCONFIG=srv-idp.1.x.local.json
 else
   REMOTECONFIG=srv-idp.2.x.remote.json
-  LOCALCONFIG=srv-idp.2.x.json
+  LOCALCONFIG=srv-idp.2.x.local.json
 fi
 
 # generate a config file from a template
