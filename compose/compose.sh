ea271b7f573f61a39377245b95a1926ffca5160d
Andrew Smith <andrew.smith@miracl.com>
2018-04-10 15:08:33 +0100

    fix for COMPOSE_CONVERT_WINDOWS_PATHS

--- compose/compose.sh
+++ compose/compose.sh
@@ -7,7 +7,8 @@ cmd="$dir/docker-compose_linux"
 
 platform=`uname`
 if [[ $platform = *"MINGW"* ]] || [[ "$platform" = *"CYGWIN"* ]]; then
-   cmd="$dir/docker-compose_windows"
+  export COMPOSE_CONVERT_WINDOWS_PATHS=1
+  cmd="$dir/docker-compose_windows"
 fi
 
 # check if our vendored versions loads correctly
