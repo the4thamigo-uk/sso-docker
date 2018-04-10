f15a6017f748069ed7339b2df9b842af52098603
Andrew Smith <andrew.smith@miracl.com>
2018-04-10 15:00:59 +0100

    default to installed docker compose installation

--- compose/compose.sh
+++ compose/compose.sh
@@ -3,12 +3,18 @@ set -e
 
 dir="$(dirname ${BASH_SOURCE[0]})"
 
-cmd="docker-compose_linux"
+cmd="$dir/docker-compose_linux"
 
 platform=`uname`
 if [[ $platform = *"MINGW"* ]] || [[ "$platform" = *"CYGWIN"* ]]; then
-   cmd="docker-compose_windows"
+   cmd="$dir/docker-compose_windows"
 fi
 
-"$dir/$cmd" -f "$dir/docker-compose.yml" "$@"
+# check if our vendored versions loads correctly
+# if not, try a local install, if one exists
+if ! $cmd --help &> /dev/null; then
+  cmd=docker-compose
+fi
+
+$cmd -f "$dir/docker-compose.yml" "$@"
 
